4d2b6822a68f609db7481ad9ceb621c9c3a01b88
METPAHYSICS <47684055+mtphyscs@users.noreply.github.com>
2021-05-19 00:16:50 -0400

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "monospace",
+  textdecoration: "none"
+
+}
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Messages from Heaven</title>
+      <h1>
+        FILENAME: MFTP.DOC
+        ============================================================
+      </h1>
+      <ul>
+        <li>Message #001 </li>
+        <li> Date : 18-May-2021 14:00</li>
+        <li> From : The Pleiadians</li>
+        <li> To : Humanity</li>
+        <li> Subject : Ascension Process, May 26th Cosmic Event</li>
+      </ul>
+      <h1>============================================================</h1>
+ <p>We are the Pleiadians and we greet you today in the love and the light of the one Supreme creator who has allowed us to co-exist and allowed you to exist in your planetary sphere at this time-space nexus. Before we start sharing our thoughts please understand that you have free will to accept or reject this information, we respect your free will and it within our mission to preserve it. Awakening awareness is rising amongst the masses on Planet Earth at this current time-space nexus the multitude of these awakening events are seeping and unfolding in front of each entity’s reality on your planet.</p>
+ <br/>
+ <p>At these times these events are meant to help you as a collective and as a species to rise up into this new level of consciousness which in essence is the new fourth-density light expression these galactic upgrades and tidal waves of light awakening for Starseeds will come on the date of 26th may 2021 known in your current timeline via the various portals in your planetary sphere that are open to allow such tidal waviest penetrate and affect your masses. </p>
+ <br/>
+ <p>Most beings are being awakened at this time, as you walk and live on your planet you can sense this rumbling changing consciousness that is affecting even the internal heart of Gaia which starts the rights of passage for the humans into these new levels of consciousness. We feel that we have given to this planet at this time a succinct message of inspiration a message that holds waves of truth these messages are meant to bring forth and to rise up that part of your inner self that has been dormant for such a long time on your planetary experience which happens on all levels of this density through the process of the veil of forgetting upon your people’s mind, which caused them to miss and forget their true inner essence.</p>
+<br/>
+<p>We are talking through this instrument and sharing our thoughts and insight in order to awaken within you that which you have forgotten this illusion that surrounds you is a reality that is temporary and constantly change in accordance with the vibratory rates of your people, in essence, what you see is a reflection of your inner vibration rate these messages are also meant to bring to you and understanding of a parallel existing reality that concurrently exists within this original reality.</p>
+ <br/>
+ <p>The cycles of transformation have taken a positive turn on your planetary sphere as we scan your planet. The waves of the new energies are going to change the very fabric of every being’s existence these planetary changes occur every 75000-year cycle In all planets in the cosmos this like clockwork these changes are meant to allow every being in such planets the opportunity to evolve faster into the higher levels of consciousness. Our primary intention with maintaining this contact has been to stir each of you from with yourself without making each uncomfortable we are here to encourage you to climb and ascend into a few peaks of the mountain ranges of consciousness that is.</p>  
+ <br/>
+ <p>Before each of you at this time there shall you find these new energetic vistas of consciousness and galactic waves of lights from the future. We are here to request each of you to use your free will and to recognize your calling and step forward as a member of this family of Light in order to accept these upcoming galactic upgrades. This however does not mean that you should force other entities to accept these upgrades in essence these upgrades will only affect those who match with their vibratory rate, similar to your so-called radio devices that match their vibration to pick up only a certain frequency while ignoring the others similarly many on your sphere whose vibration is below the level of acceptance as know by your kind who referred to the so-called consciousness chart system will not receive such upgrades these beings will again have to wait another 75000 years year cycle to avail of this opportunity to access these waves of upgrades in consciousness.</p> 
+ <br/>
+ <p>Those who vibrate above and are at this level of acceptance will be able to receive these upgrades, the gifts of moving into this higher octave of understating and this blending of dimensions and creation of new territory will lead to everyone through greater understating of death, your light will be needed, your light represents what you know at these time the death of the old earth starts and new discoveries and the miracles of operating the physical vehicle in these times of change and the transition will involve the death of the old world as you know as the third-density world as this planet moves closer to these days of great change each of you will be called upon to stand as the pillars of light.
+</p>
+<br/>
+<p>You will show the way in times when people are desperate because the old ways are no longer solutions they no longer fit they no longer apply. No identity can escape of run or hide as the waves of transformation will weave through the earth planes and start to bring in and awaken those who are yet asleep on your planetary sphere as the days and the nights begin to come and go the will begin to show the beings on your planet and reveal the truth behind the ancient prophecies many of which will begin alive on your planet, these prophecies will start to b more colorfully latent in their own sense of lessons that are required to be learned by every entity on your planetary sphere.</p>
+<br/>
+<p>Within the next few days and years to come many beings on planet Earth will be catapulted faster than the speed of light and will begin to experience the turret within their own selves this will also bring forth new abilities and talents along with information that will come to many of you in simple natural ways because you were already part of your inner truth of who you truly are.</p>
+<br/>
+<p> Let us answer the second query placed by this instrument regarding the new earth and let us explain to you in your vibratory sound complex terms please understand that we will try to accurately describe how this new earth will come about on your planetary sphere. Once upon another planet in a certain star system, a similar planet like the earth had been transitioning into the beginning phases of the fourth-density positive polarity consciousness; the same process that which is occurring on your planet was occurring also at that planet.</p> 
+<br/>
+<p>During this transformation period there was an entity who asked its so-called parent entity also known as mother in your language terms about what will the new planet be like much similar to your query of what will the new earth be like the answer to which this mother entity replied stating by bringing a bag of rice as you know it and told the answer was in the bad of rice however this child entity could not fully grasp the complexity of the answer and asked further to explain as to how this bag of rice relates to the coming new earth further the mother entity replied stating that just as in this bag of rice there are good rices grains and some rotten rice grains and some small unwanted pieces of stones or other materials which are separated by the process of winnowing the same is going to happen on the planet which will separate the unwanted parts from the good parts eventually leaving behind only the good time which will comprise the new earth in essence each rice particle can be equated to an entity on your planetary sphere. Therefore beloveds we leave you now hoping that we have answered your queries at this time-space nexus we leave you now in the love and the light of the prime creator see you again.</p>  
+    </main>
+  );
+};
+
+export default IndexPage;
